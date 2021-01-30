a46dd8df34fed0d24ba0462c355523fd389790ae
Bilal HAider <bilalhaider1311@gmail.com>
2021-01-30 20:40:26 +0500

    app update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div >
-      hello bilal I am online me boo
+      hello bilal me fool
     </div>
   );
 }
